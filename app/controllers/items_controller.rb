7599a63405de2e861b66b3ab527259dae5bcbd5f
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-04-22 20:44:17 +0900

    修正。(商品詳細画面の実装を先にしたため)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index]
-  before_action :set_item, only: [:show]
+  #before_action :set_item, only: [:show]
   def index
     @items = Item.all.order("created_at DESC")
   end
