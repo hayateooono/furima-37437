634435b78e6c79573830ecf7d8f1f5142bc48b75
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-04-23 11:18:41 +0900

    記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index,:show]
-  before_action :set_item, only: [:show]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -16,11 +15,12 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
     def show
+      @item = Item.find(params[:id])
     end
 
-  end
 
   private
 
@@ -28,7 +28,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:info,:price,:category_id,:condition_id,:pay_for_sipping_id,:prefecture_id,:shipping_day_id,:image,:user).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
 end
