57e0e69765c145d665d2a1ba9e1750eb4e180e5d
hayateooono <99227043+hayateooono@users.noreply.github.com>
2022-04-23 11:33:05 +0900

    詳細画面実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
+<<<<<<< HEAD
   before_action :authenticate_user!,  except: [:index,:show]
+=======
+  before_action :authenticate_user!,  except: [:index]
+  #before_action :set_item, only: [:show]
+>>>>>>> parent of 35c5c32 (詳細画面実装)
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,7 +23,11 @@ class ItemsController < ApplicationController
   end
 
     def show
+<<<<<<< HEAD
       @item = Item.find(params[:id])
+=======
+      #@item = Item.find(params[:id])
+>>>>>>> parent of 35c5c32 (詳細画面実装)
     end
 
 
@@ -28,4 +37,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:info,:price,:category_id,:condition_id,:pay_for_sipping_id,:prefecture_id,:shipping_day_id,:image,:user).merge(user_id: current_user.id)
   end
 
+<<<<<<< HEAD
+=======
+  #def set_item
+    #@item = Item.find(params[:id])
+  #end
+>>>>>>> parent of 35c5c32 (詳細画面実装)
 end
